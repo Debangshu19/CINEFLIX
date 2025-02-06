7f7e2eb49fbf7a03ad98b02f1ea8049c8bd0a288
Debangshu Dey <138014640+Debangshu19@users.noreply.github.com>
2025-02-06 19:46:51 +0530

    Update generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,4 +1,4 @@
-const jwt = require('jsonwebtoken');
+/*const jwt = require('jsonwebtoken');
 const ENV_VARS = require('../config/envVars');
 
 module.exports.generateTokenAndSetCookie = (userId, res) => {
@@ -12,4 +12,20 @@ module.exports.generateTokenAndSetCookie = (userId, res) => {
     });
     res.locals.T = token;
     return token;
+};
+*/
+const jwt = require('jsonwebtoken');
+const ENV_VARS = require('../config/envVars');
+
+module.exports.generateTokenAndSetCookie = (userId, res) => {
+    const token = jwt.sign({ userId }, ENV_VARS.JWT_SECRET, { expiresIn: '14d' });
+
+    res.cookie('jwt-cineflix', token, {
+        maxAge: 14 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none' // 'none' allows cross-origin
+    });
+
+    return token; // Return the token so it can be sent in the response
 };
