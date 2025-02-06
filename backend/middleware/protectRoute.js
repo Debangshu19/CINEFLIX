a61592d03b6a4af6d19fe9df2c4d6af668c533f9
Debangshu Dey <138014640+Debangshu19@users.noreply.github.com>
2025-02-06 19:19:24 +0530

    Update protectRoute.js

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -3,7 +3,6 @@ const User = require('../models/user.model');
 const ENV_VARS = require('../config/envVars');
 
 module.exports.protectRoute = async (req, res, next) => {
-    /*
     try{
         const token = req.cookies['jwt-cineflix'] || res.locals.T;
         console.log("Token: ", token);
@@ -24,5 +23,4 @@ module.exports.protectRoute = async (req, res, next) => {
         console.log("Error in protectRoute middleware: ", err);
         res.status(500).json({success:false, message: "Internal server error"});
     }
-    */
 };
