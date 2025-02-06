3e04bc5ae0c8904baea6cbdd14aa106fb95e8530
Debangshu Dey <138014640+Debangshu19@users.noreply.github.com>
2025-02-06 21:46:54 +0530

    Update MovieSlider.jsx

--- frontend/src/components/MovieSlider.jsx
+++ frontend/src/components/MovieSlider.jsx
@@ -17,7 +17,7 @@ const MovieSlider = ({category}) => {
 
     useEffect(() => {
         const getContent = async () => {
-            const res = await axios.get(`https://cineflix-0bd3.onrender.com/api/v1/${contentType}/${category}`);
+            const res = await axios.get(`https://cineflix-0bd3.onrender.com/api/v1/${contentType}/${category}`, {withCredentials: true});
             setContent(res.data.content);
         };
 
