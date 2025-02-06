fcbe151e23a9dac8df3326530bd964203e730b80
Debangshu Dey <138014640+Debangshu19@users.noreply.github.com>
2025-02-06 16:53:46 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://cineflix-03vm.onrender.com",
       }
     }
   }
