0256de1363da827935063268429f5b294bbd3509
Debangshu Dey <138014640+Debangshu19@users.noreply.github.com>
2025-02-06 17:25:05 +0530

    Update SearchPage.jsx

--- frontend/src/pages/SearchPage.jsx
+++ frontend/src/pages/SearchPage.jsx
@@ -23,7 +23,7 @@ const SearchPage = () => {
 	const handleSearch = async (e) => {
 		e.preventDefault();
 		try {
-			const res = await axios.get(`/api/v1/search/${activeTab}/${searchTerm}`);
+			const res = await axios.get(`https://cineflix-03vm.onrender.com/api/v1/search/${activeTab}/${searchTerm}`);
             console.log(res);
 			setResults(res.data.content);
 		} catch (error) {
