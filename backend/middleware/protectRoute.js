d13cf6642a9c9d105d85caa225dd6393275392b5
Debangshu Dey <138014640+Debangshu19@users.noreply.github.com>
2025-02-06 19:26:50 +0530

    Update protectRoute.js

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -4,7 +4,7 @@ const ENV_VARS = require('../config/envVars');
 
 module.exports.protectRoute = async (req, res, next) => {
     try{
-        const token = req.cookies['jwt-cineflix'] || res.locals.T;
+        const token = req.cookies['jwt-cineflix'] || req.headers.authorization?.split(' ')[1];
         console.log("Token: ", token);
         if(!token){
             return res.status(401).json({success:false, message: "Unauthorized - no token provided"});
