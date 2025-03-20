894b25bcfd5f9cbd1fb72cd39026b2dcfb4fdd6a
Debangshu Dey <138014640+Debangshu19@users.noreply.github.com>
2025-03-20 13:41:14 +0530

    Update MovieSlider.jsx

--- frontend/src/components/MovieSlider.jsx
+++ frontend/src/components/MovieSlider.jsx
@@ -46,6 +46,7 @@ const MovieSlider = ({category}) => {
 				{formattedCategoryName} {formattedContentType}
 			</h2>
             <div className='flex space-x-4 overflow-x-scroll scrollbar-hide' ref={sliderRef}>
+		    {/*
                 {content?.map((item) => (
                     <Link to={`/watch/${item.id}`} className='min-w-[250px] relative group' key={item.id}>
                         <div className='rounded-lg overflow-hidden'>
@@ -58,6 +59,19 @@ const MovieSlider = ({category}) => {
 						<p className='mt-2 text-center'>{item.title || item.name}</p>
                     </Link>
                 ))}
+		*/}
+		    {content?.filter((item) => item.backdrop_path).map((item) => ( // Filter items with valid backdrop_path
+			    <Link to={`/watch/${item.id}`} className="min-w-[250px] relative group" key={item.id}>
+			      <div className="rounded-lg overflow-hidden">
+			        <img
+			          src={SMALL_IMG_BASE_URL + item.backdrop_path}
+			          alt="Movie image"
+			          className="transition-transform duration-300 ease-in-out group-hover:scale-125"
+			        />
+			      </div>
+			      <p className="mt-2 text-center">{item.title || item.name}</p>
+			    </Link>
+			  ))}
             </div>
 
             {showArrows && (
